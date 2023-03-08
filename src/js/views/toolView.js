c41ca8bfac0ecaa8c41253ce78338eef444665bd
Anthony Campos <Anthonyck89@hotmail.com>
2023-03-08 09:41:14 -0800

    dropdown menu pt1

--- src/js/views/toolView.js
+++ src/js/views/toolView.js
@@ -9,8 +9,34 @@ class ToolView extends View {
     this._toolType = toolType;
   } // end setTool
 
+  addHandlerNav(handler) {
+    this._parentEl.addEventListener('click', function (event) {
+      const btn = event.target.closet('.nav__dropdown');
+      if (!btn) {
+        return;
+      }
+
+      btn.toggle('hidden');
+    });
+  }
+
   _generateMarkup() {
     return `
+      <div class="nav__dropdown hidden">
+        <button class="nav__dropdown">
+          <img
+            src="src/img/menu-symbol.svg"
+            alt="menu button, click to open menu"
+            class="menu__svg"
+          />
+        </button>
+        <h2 class="calc_list--title">Calculator</h2>
+        <ul class="calc_list">
+        <li class="calc_item">
+          Standard
+        </li>
+        </ul>
+      </div> 
       <nav class="nav">
         <button class="nav__btn">
           <img
@@ -19,7 +45,7 @@ class ToolView extends View {
             class="menu__svg"
           />
         </button>
-        <h2>${this._toolType}</h2>
+        <h2 class="tool__title">${this._toolType}</h2>
       </nav>
       ${LAYOUT_MAP.get(this._toolType)}
       `;
@@ -33,3 +59,6 @@ class ToolView extends View {
 } // end ToolView
 
 export default new ToolView();
+
+{
+}
