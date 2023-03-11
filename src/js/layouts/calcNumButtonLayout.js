64478344451aeb00db7becf9bc6650423d5af725
Anthony Campos <Anthonyck89@hotmail.com>
2023-03-10 20:43:18 -0800

    style for converter set

--- src/js/layouts/calcNumButtonLayout.js
+++ src/js/layouts/calcNumButtonLayout.js
@@ -1,16 +1,16 @@
 const calcNumButtonLayout = `
-<button class="btn number" value="7">7</button>
-<button class="btn number" value="8">8</button>
-<button class="btn number" value="9">9</button>
-<button class="btn number" value="4">4</button>
-<button class="btn number" value="5">5</button>
-<button class="btn number" value="6">6</button>
-<button class="btn number" value="1">1</button>
-<button class="btn number" value="2">2</button>
-<button class="btn number" value="3">3</button>
-<button class="btn number" value="+/-">&plusmn;</button>
-<button class="btn number" value="0">0</button>
-<button class="btn number" value=".">.</button>
+<button class="btn" value="7">7</button>
+<button class="btn" value="8">8</button>
+<button class="btn" value="9">9</button>
+<button class="btn" value="4">4</button>
+<button class="btn" value="5">5</button>
+<button class="btn" value="6">6</button>
+<button class="btn" value="1">1</button>
+<button class="btn" value="2">2</button>
+<button class="btn" value="3">3</button>
+<button class="btn" value="+/-">&plusmn;</button>
+<button class="btn" value="0">0</button>
+<button class="btn" value=".">.</button>
 `;
 
 export default calcNumButtonLayout;
--- src/js/layouts/converterButtonLayout.js
+++ src/js/layouts/converterButtonLayout.js
@@ -5,18 +5,18 @@ const converterButtonLayout = `
 <button class="btn cmd btn--converter" value="clear entry">CE</button>
 <button class="btn cmd btn--converter" value="back">${backIcon}
 </button>
-<button class="btn number btn--converter" value="7">7</button>
-<button class="btn number" value="8">8</button>
-<button class="btn number" value="9">9</button>
-<button class="btn number" value="4">4</button>
-<button class="btn number" value="5">5</button>
-<button class="btn number" value="6">6</button>
-<button class="btn number" value="1">1</button>
-<button class="btn number" value="2">2</button>
-<button class="btn number" value="3">3</button>
+<button class="btn btn--converter" value="7">7</button>
+<button class="btn btn--converter" value="8">8</button>
+<button class="btn btn--converter" value="9">9</button>
+<button class="btn btn--converter" value="4">4</button>
+<button class="btn btn--converter" value="5">5</button>
+<button class="btn btn--converter" value="6">6</button>
+<button class="btn btn--converter" value="1">1</button>
+<button class="btn btn--converter" value="2">2</button>
+<button class="btn btn--converter" value="3">3</button>
 <div class="btn_spacer"></div>
-<button class="btn number" value="0">0</button>
-<button class="btn number" value=".">.</button>
+<button class="btn btn--converter" value="0">0</button>
+<button class="btn btn--converterr" value=".">.</button>
 `;
 
 export default converterButtonLayout;
