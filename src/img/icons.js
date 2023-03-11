9dbd0b1596809841038df4d7b9547f6fbc537b67
Anthony Campos <Anthonyck89@hotmail.com>
2023-03-10 19:33:03 -0800

    icon file made

--- src/img/icons.js
+++ src/img/icons.js
@@ -14,3 +14,20 @@ export const backIcon = `
   />
   </svg>
   `;
+
+export const dropdownIcon = `
+  <svg
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+  viewBox="0 0 24 24"
+  stroke-width="1.5"
+  stroke="currentColor"
+  class="svg_dropdown"
+  >
+  <path
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+  />
+  </svg>
+`;
--- src/js/layouts/converterLayout.js
+++ src/js/layouts/converterLayout.js
@@ -1,4 +1,5 @@
 import converterButtonLayout from './converterButtonLayout.js';
+import { dropdownIcon } from '../../img/icons.js';
 
 const converterCmdLayout = `
   <button class="btn cmd" value="clear entry">CE</button>
@@ -16,10 +17,7 @@ const converterLayout = `
       <p class="display_unit">0</p>
       </div>
       <button class="converter_unit_dropdown ">
-        Centimeters
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="svg_dropdown">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-        </svg>
+        Centimeters ${dropdownIcon}
       </button>  
     </div>
     <div class="convert_unit_section">
@@ -27,10 +25,7 @@ const converterLayout = `
       <p class="display_unit">0</p>
       </div>
       <button class="converter_unit_dropdown">
-        Centimeters
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="svg_dropdown">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-        </svg>
+        Centimeters ${dropdownIcon}
       </button>  
     </div>
     <div class="convert_unit_section">
